ac08cfd3e37f3d8a66dfdd7b814dfbd129de9716
jake <gerion@gmail.com>
2024-01-10 11:38:44 +0900

    파일분리 및 펑션명칭 재정의

--- app.js
+++ app.js
@@ -1,43 +1,11 @@
+import { createStore } from './redux.js';
+import { reducer } from './reducer.js';
 
-function createStore(worker) {
-    let state;
-    let handlers = [];
-
-    function send(action) {
-        state = worker(state, action);
-        handlers.forEach(handler => handler());
-    }
-
-    function subscribe(handler) {
-        handlers.push(handler);
-    }
-
-    function getState() {
-        return state;
-    }
-
-    return {
-        send,
-        getState,
-        subscribe,
-    };
-}
-
-function worker(state = { count: 0 }, action) {
-
-    switch (action.type) {
-        case 'increase':
-            return {...state, count: state.count + 1};
-        default:
-            return { ...state };
-    }
-}
-
-const store = createStore(worker);
+const store = createStore(reducer);
 
 store.subscribe(function (){
     console.log(store.getState());
 });
 
-store.send({ type: 'increase' });
-store.send({ type: 'increase' });
+store.dispatch({ type: 'increase' });
+store.dispatch({ type: 'increase' });
